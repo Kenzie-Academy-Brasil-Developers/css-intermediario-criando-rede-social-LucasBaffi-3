3cb91d25d623c54f09d30a5b375e5f181a999ac9
Baffi <lucasrenatobaffi@hotmail.com>
2022-09-25 15:58:56 -0300

    logica dom posts e ajustes na estilizacao

--- script/database/posts.js
+++ script/database/posts.js
@@ -1,25 +1,77 @@
-//Posts dos usuários
-const posts = [
-  {
-    id_post: 1, //id único de cada post
-    user: 2, //id do usuário que fez o post, você pode acessa-lo no array users
-    //título do post
-    title:
-      "Empresa de Tecnologia da Informação abre vagas para programa de estágio",
-    //texto do post
-    text: "A Framework Digital, empresa mineira especializada em Tecnologia da Informação, irá iniciar o seu sexto programa de estágio, com o prazo de inscrições entre os dias 08 e 28 de agosto. O programa é conhecido como Framework Padawans, com inspiração nos filmes Star Wars. Nas histórias, os iniciantes fazem treinamentos para se tornar cavaleiros Jedi, que compõem o lado bom da força.",
-  },
-  {
-    id_post: 2,
-    user: 4,
-    title:
-      "Programa de estágio abre vagas em Segurança da Informação com regime remoto",
-    text: "Em Segurança da Informação, as vagas são destinadas às áreas de Security Operations Center (SOC), Administração de Dispositivos de Segurança (ADS), Resposta a Incidentes (RI), Segurança e Privacidade e Consultoria Técnica. O candidato interessado deverá estar matriculado em um curso superior em Ciência da Computação, Segurança da Informação, Tecnologia da Informação e afins, com previsão de conclusão do curso de fevereiro de 2023 a fevereiro de 2025.",
-  },
-  {
-    id_post: 3,
-    user: 5,
-    title: "O que é programação orientada a objetos e programação funcional",
-    text: "A Programação Funcional é uma orientação focada na programação declarativa. Conhecendo mais a programação funcional a partir de códigos podemos nos deparar de primeira com o conceito mais central da programação funcional, que é o conceito de funções puras, isso significa que o retorno de uma função deve depender apenas dos seus parâmetros de entrada. Com classes podemos editar os valores das propriedades dos objetos criados ou ainda criar métodos para essas propriedades, ainda por cima podemos definir se vão ser públicos (vão para o objeto) ou estáticos (não são instanciados, ou seja, não vão para o objeto), e isso tem seu lado bom e ruim.",
-  },
-];
+
+{/* <li >
+<div class="perfil">
+  <img src="./assets/img/Ellipse 1.png" alt="" />
+<div>
+  <h3>Samuel Leao</h3>
+  <p>Front end Enginner</p>
+</div>
+</div>
+
+<div class="card">
+  <h3>laskdhakshdkjahsdksa</h3>
+  <p>laskjhdlkasjdlkahskldhakshd</p>
+</div>
+
+<div class= "div-buttons">
+  <button class="button-openPost">Abrir Post</button>
+  <input type="radio" />
+</div> */}
+
+
+let renderPost = (users) => {
+
+let li = document.createElement('li')
+let divPerfil = document.createElement('div')
+let img = document.createElement('img')
+let div = document.createElement('div')
+let h3 = document.createElement('h3')
+let p = document.createElement('p')
+let divCard = document.createElement('div')
+let h3Card = document.createElement('h3')
+let pCard = document.createElement('p')
+let divButton = document.createElement('div')
+let button = document.createElement('button')
+let input = document.createElement('input')
+
+img.src = users.img
+h3.innerText = users.user
+p.innerText = users.stack
+h3Card.innerText = users.title
+pCard.innerText = users.text
+button.innerText = 'Abrir post'
+
+divPerfil.classList.add('perfil')
+divCard.classList.add('card')
+divButton.classList.add('div-buttons')
+img.classList.add('img-post')
+button.classList.add('button-openPost')
+input.type = ('radio')
+
+
+
+divPerfil.append(img, div)
+div.append(h3, p)
+divCard.append(h3Card, pCard)
+
+divButton.append(button, input)
+
+li.append(divPerfil, divCard, divButton)
+
+return li
+
+}
+
+
+let ulPosts = document.querySelector('.posts')
+let listaPosts = (listarPosts) => { 
+
+  ulPosts.innerHTML = ""
+
+listarPosts.forEach((element) => {
+  let posts = renderPost(element)
+  ulPosts.append(posts)
+
+});
+}
+listaPosts(users)
